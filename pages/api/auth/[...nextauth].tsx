9a5010f5fecc7a43c9a3eff7ac7675337f8e78ff
Thomas Henderson <tbhenderson55@yahoo.com>
2021-12-12 21:21:38 +0000

    rm fb login

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth/next";
-import FacebookProvider from "next-auth/providers/facebook";
+// import FacebookProvider from "next-auth/providers/facebook";
 // import EmailProvider from "next-auth/providers/email";
 import GithubProvider from "next-auth/providers/github";
 // import Auth0Provider from "next-auth/providers/auth0";
@@ -12,10 +12,10 @@ export default NextAuth({
     //   server: process.env.EMAIL_SERVER,
     //   from: process.env.EMAIL_FROM,
     // }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_ID,
+    //   clientSecret: process.env.FACEBOOK_SECRET,
+    // }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
