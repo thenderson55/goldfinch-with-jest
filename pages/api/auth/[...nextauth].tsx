657b8e210bba081e8d9134df57f17e85c4b03378
Thomas Henderson <tbhenderson55@yahoo.com>
2021-12-15 20:25:31 +0000

    authO

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -2,7 +2,7 @@ import NextAuth from "next-auth/next";
 import FacebookProvider from "next-auth/providers/facebook";
 // import EmailProvider from "next-auth/providers/email";
 import GithubProvider from "next-auth/providers/github";
-// import Auth0Provider from "next-auth/providers/auth0";
+import Auth0Provider from "next-auth/providers/auth0";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import { clientPromise } from "../../../lib/mongodb";
 
@@ -12,10 +12,10 @@ export default NextAuth({
     //   server: process.env.EMAIL_SERVER,
     //   from: process.env.EMAIL_FROM,
     // }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_ID,
+    //   clientSecret: process.env.FACEBOOK_SECRET,
+    // }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
@@ -29,6 +29,12 @@ export default NextAuth({
     //   // @ts-ignore
     //   domain: process.env.AUTH0_DOMAIN,
     // }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER,
+      // authorizationUrl: `https://${process.env.AUTH0_DOMAIN}/authorize?response_type=code&prompt=login`,
+    }),
   ],
   adapter: MongoDBAdapter(clientPromise),
   secret: process.env.NEXTAUTH_SECRET,
---
+++ pages/api/auth/signout.tsx
@@ -0,0 +1,21 @@
+async function handleLogout(req, res) {
+  console.log("SIIGGN  OOUUT");
+  const returnTo = encodeURI("http://localhost:3000/");
+  res.redirect(
+    // `https://${process.env.AUTH0_ISSUER}/v2/logout?federated&returnTo=${returnTo}`
+    `${process.env.AUTH0_ISSUER}/v2/logout?returnTo=${returnTo}`
+    // `https://${process.env.AUTH0_ISSUER}/v2/logout?client_id=${process.env.AUTH0_CLIENT_ID}&returnTo=${returnTo}`
+    // `https://${process.env.AUTH0_ISSUER}/v2/logout?client_id=${process.env.AUTH0_CLIENT_ID}&returnTo=${returnTo}`
+  );
+
+  // res.redirect(returnTo);
+}
+
+export default async function logout(req, res) {
+  try {
+    await handleLogout(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(error.status || 400).end(error.message);
+  }
+}
