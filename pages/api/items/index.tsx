dfdac120ba4fa91a3e3d2fbe9d78aef58a079fd7
Thomas Henderson <tbhenderson55@yahoo.com>
2021-12-23 15:37:37 +0000

    try with connectToDatabase

--- pages/api/items/index.tsx
+++ pages/api/items/index.tsx
@@ -5,9 +5,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const client = await clientPromise;
-  // const { db } = await connectToDatabase();
-  const db = client.db("goldFinchDb");
+  // const client = await clientPromise;
+  // const db = client.db("goldFinchDb");
+  const { db } = await connectToDatabase();
   // console.log("DB: ", db);
 
   const limit = parseInt(req.query.limit as string);
