96463847a8371911cd90514ce696128ab13afa64
Thomas Henderson <tbhenderson55@yahoo.com>
2021-12-28 15:23:58 +0000

    ts-jest added with sum test

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,42 @@ const customJestConfig = {
     // Handle module aliases (this will be automatically configured for you soon)
     '^@/components/(.*)$': '<rootDir>/components/$1',
   },
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  globals: {
+  'ts-jest': {
+    tsconfig: './tsconfig.jest.json',
+  },
+  },
+  // setupFilesAfterEnv:  ['<rootDir>/jest.setup.js'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)
+
+// module.exports = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'jsdom',
+//   globals: {
+//     'ts-jest': {
+//       tsconfig: './tsconfig.jest.json',
+//     },
+//   },
+//   setupFilesAfterEnv:  ['<rootDir>/jest.setup.js'],
+//   coverageThreshold: {
+//     global: {
+//       branches: 100,
+//       functions: 100,
+//       lines: 100,
+//       statements: 100,
+//     },
+//   },
+// };
