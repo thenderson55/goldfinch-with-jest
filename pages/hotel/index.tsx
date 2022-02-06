3bdb10545893ae109215019530b3303d91a02a53
Thomas Henderson <tbhenderson55@yahoo.com>
2022-02-06 19:38:19 +0000

    header basics

--- pages/hotel/index.tsx
+++ pages/hotel/index.tsx
@@ -2,20 +2,43 @@ import React from "react";
 import Link from "next/link";
 import styles from "./index.module.scss";
 import Image from "next/image";
-import MagnifyingGlass from "../../svgs/SvgMagnifyingGlass";
+import SvgMagnifyingGlass from "../../svgs/SvgMagnifyingGlass";
+import SvgCake from "../../svgs/SvgCake";
+import seamus from "../../public/seamus.jpg";
 
 function Hotel() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <header className={styles.header}>
-          {/* <Image src={dalmation} alt="Dalmation" className={styles.headerLogo} /> */}
+          <SvgCake className={styles.headerIcon} />
+
+          {/* <Image src={seamus} alt="seamus" className={styles.headerLogo} /> */}
           <form action="" className={styles.search}>
             <input type="text" className={styles.searchInput} />
             <button className={styles.searchButton}>
-              <MagnifyingGlass className={styles.searchIcon} />
+              <SvgMagnifyingGlass className={styles.searchIcon} />
             </button>
           </form>
+          <nav className={styles.userNav}>
+            <div className={styles.userNavIconBox}>
+              <SvgCake className={styles.userNavIcon} />
+              <span className={styles.userNavNotification}>5</span>
+            </div>
+            <div className={styles.userNavIconBox}>
+              <SvgCake className={styles.userNavIcon} />
+              <span className={styles.userNavNotification}>14</span>
+            </div>
+            <div className={styles.userNavUser}>
+              <div className={styles.userNavAvatarBox}>
+                <Image
+                  src={seamus}
+                  alt="user photo"
+                  className={styles.userNavAvatar}
+                />
+              </div>
+            </div>
+          </nav>
         </header>
         <div className={styles.content}>
           <nav className={styles.sidebar}>NAV</nav>
