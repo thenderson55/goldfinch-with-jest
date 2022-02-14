e9c46f1411c9ac9a07b26f44d31192dd2d39be4a
Thomas Henderson <tbhenderson55@yahoo.com>
2022-02-14 17:41:02 +0000

    grid

---
+++ pages/grid/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+function Grid() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>Header</header>
+      <div className={styles.box1}>Box 1</div>
+      <div className={styles.box2}>Box 2</div>
+      <div className={styles.box3}>Box 3</div>
+      <div className={styles.main}>Main</div>
+      <div className={styles.sidebar}>Sidebar</div>
+      <footer className={styles.footer}>Footer</footer>
+    </div>
+  );
+}
+
+export default Grid;
