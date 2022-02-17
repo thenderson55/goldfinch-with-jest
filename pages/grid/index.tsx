71ac10882489a57a5add9730bb9c36b621db706d
Thomas Henderson <tbhenderson55@yahoo.com>
2022-02-17 16:02:51 +0000

    story content

--- pages/grid/index.tsx
+++ pages/grid/index.tsx
@@ -17,7 +17,20 @@ function Grid() {
         <Feature />
       </section>
       <div className={styles.storyPictures}>Story-pictures</div>
-      <div className={styles.storyContent}>Story-content</div>
+      <div className={styles.storyContent}>
+        <h3 className={styles.storyContentTitle}>HAPPY CLAPPY PEOPLE</h3>
+        <h2 className={styles.storyContentQuote}>
+          &ldquo;The best desicions of our lives&ldquo;
+        </h2>
+        <div className={styles.storyContentText}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit in harum quia optio, libero enim fugiat ad dolore
+          exercitationem beatae.
+        </div>
+        <button className={styles.storyContentButton}>
+          FIND YOUR OWN HOME
+        </button>
+      </div>
       <section className={styles.homes}>Homes</section>
       <section className={styles.gallery}>Gallery</section>
       <footer className={styles.footer}>Footer</footer>
