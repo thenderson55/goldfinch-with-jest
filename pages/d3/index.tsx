1e27b2f38edb449404cc612bd35a7acb280fa25d
Thomas Henderson <tbhenderson55@yahoo.com>
2022-05-16 14:07:06 +0400

    d3 added and arc used

--- pages/d3/index.tsx
+++ pages/d3/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { arc } from 'd3';
 
 function D3() {
   const width = 960;
@@ -8,29 +9,30 @@ function D3() {
   const centerY = height / 2;
   const eyeRadius = 50;
   const eyeOffsetX = 100;
-  const eyeOffsetY = 50;
+  const eyeOffsetY = 80;
+  const mouthWidth = 10;
+  const mouthRadius = 150;
+
+  const mouthArc = arc()
+    .innerRadius(mouthRadius)
+    .outerRadius(mouthRadius + mouthWidth)
+    .startAngle(Math.PI / 2)
+    .endAngle(Math.PI * 1.5);
 
   return (
     <div>
       <svg width={width} height={height}>
-        <circle
-          cx={centerX}
-          cy={centerY}
-          r={centerY - strokeWidth / 2}
-          fill="yellow"
-          stroke="black"
-          strokeWidth={strokeWidth}
-        ></circle>
-        <circle
-          cx={centerX - eyeOffsetX}
-          cy={centerY - eyeOffsetY}
-          r={eyeRadius}
-        ></circle>
-        <circle
-          cx={centerX + eyeOffsetX}
-          cy={centerY - eyeOffsetY}
-          r={eyeRadius}
-        ></circle>
+        <g transform={`translate(${centerX}, ${centerY})`}>
+          <circle
+            r={centerY - strokeWidth / 2}
+            fill="yellow"
+            stroke="black"
+            strokeWidth={strokeWidth}
+          />
+          <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+          <circle cx={+eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+          <path d={mouthArc()} />
+        </g>
       </svg>
     </div>
   );
