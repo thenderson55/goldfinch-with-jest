c6a32d9e2cab91e8c841325f965303bbefc7e007
Thomas Henderson <tbhenderson55@yahoo.com>
2023-01-07 17:05:22 +0000

    marketing page for tago

---
+++ pages/tago/marketing/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function index() {
+  return (
+    <div style={{ margin: 50 }}>
+      <h1>Tago</h1>
+      <p>
+        An app to take photos of places that you may want to go or need a visual
+        record of that can be shown on a map.
+      </p>
+      <p>It is amazing - please doownload it!</p>
+    </div>
+  );
+}
+
+export default index;
