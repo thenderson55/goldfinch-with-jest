86fe2bff0702c9e73180e30de775adb0b7521be5
Thomas Henderson <tbhenderson55@yahoo.com>
2023-02-18 07:24:57 +0000

    Float

--- components/Three/MyText.tsx
+++ components/Three/MyText.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-// import { extend } from '@react-three/fiber';
-// import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
-// import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
-import { Text3D } from '@react-three/drei';
+import { Float, Text3D } from '@react-three/drei';
 import pressStart from '../../assets/fonts/PressStart2P_Regular.json';
 
 function MyText() {
   return (
-    <Text3D font={pressStart} position={[-11, 0, 3]}>
-      Thomas Henderson
-      <meshNormalMaterial />
-    </Text3D>
+    <Float
+      // rotation={[3, 5, 6]}
+      speed={3}
+      floatIntensity={5}
+    >
+      <Text3D
+        // @ts-ignore
+        font={pressStart}
+        position={[-11, 0, 7]}
+        bevelEnabled
+        bevelThickness={0.3}
+      >
+        Thomas Henderson
+        <meshNormalMaterial />
+      </Text3D>
+    </Float>
   );
 }
 
