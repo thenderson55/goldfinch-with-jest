6fdfc1a67798550412d0e45dcc332f64a874ef63
Thomas Henderson <tbhenderson55@yahoo.com>
2023-02-18 08:09:54 +0000

    spin

--- components/Three/MyText.tsx
+++ components/Three/MyText.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import { Float, Text3D } from '@react-three/drei';
+import React, { useRef } from 'react';
+import { Center, Float, Text3D } from '@react-three/drei';
 import pressStart from '../../assets/fonts/PressStart2P_Regular.json';
+import { useFrame } from '@react-three/fiber';
+import { Mesh } from 'three/src/Three';
 
 function MyText() {
+  const meshRef = React.useRef<Mesh>();
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+    }
+  });
+
   return (
-    <Float
-      // rotation={[3, 5, 6]}
-      speed={3}
-      floatIntensity={5}
-    >
-      <Text3D
-        // @ts-ignore
-        font={pressStart}
-        position={[-11, 0, 7]}
-        bevelEnabled
-        bevelThickness={0.3}
+    <Center position={[0, 0, 7]}>
+      <Float
+        // rotation={[3, 5, 6]}
+        speed={3}
+        floatIntensity={5}
       >
-        Thomas Henderson
-        <meshNormalMaterial />
-      </Text3D>
-    </Float>
+        <Text3D
+          ref={meshRef}
+          // @ts-ignore
+          font={pressStart}
+          bevelEnabled
+          bevelThickness={0.3}
+        >
+          Thomas Henderson
+          <meshNormalMaterial />
+        </Text3D>
+      </Float>
+    </Center>
   );
 }
 
